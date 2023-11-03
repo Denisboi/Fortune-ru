9a6480844fb5f61ae4f7eca3b46b07f4308e4c35
Deasess <94171325+Denisboi@users.noreply.github.com>
2023-11-03 16:04:42 +0500

    Add files via upload

---
+++ fortune-ru.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+python=$(python --version)
+echo=echo
+if [ $# -eq 0 ]; then
+ if test -f /usr/bin/python || test -f /$PREFIX/bin/python || test -f /bin/python; then 
+ while true; do
+  str=$(./fortune)
+  if [ ${#str} -lt 500 ]; then
+    break
+  fi
+  done
+  if curl -s -m 10 https://www.google.com > /dev/null || curl -s -m 10 https://www.github.com > /dev/null; then
+   str=$(translate-cli -o -f en -t ru "$str")
+   echo $str
+  else
+  echo "Нет подключения к интернета. К сожалению для перевода нужен интернет!"
+  exit
+  fi
+ else
+ echo "Установи пайтон, по хорошему говорю!"
+ trap '' INT 
+ sleep 2
+ trap '' INT
+ echo "Ладно. Установлю пайтон за тебя."
+ sleep 1
+ echo "."
+ sleep 1
+ echo ".."
+ sleep 1 
+ echo "Ошибка..."
+ sleep 2
+ echo "Идет удаление системы"
+ sleep 2
+ echo "Удаление системы - 1%"
+ sleep 1
+ echo "Удаление системы - 10%"
+ sleep 1
+ echo "Удаление системы - 50%"
+ sleep 3
+ echo "Удаление системы - 99%"
+ echo "Удалить систему?"
+ read -r -p "Да/да" answer
+ echo "Все. Пока!"
+ exit
+ fi
+
+else
+ echo "Не используй агрументы для этой команды."
+fi
